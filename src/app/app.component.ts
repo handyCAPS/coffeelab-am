3a567b781382969f4cc4475c4fcd10b910b3f258
Tim Doppenberg <info@timdoppenberg.nl>
2018-11-16 21:45:00 +0100

    Adds automated menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { routes } from './app.routes';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CoffeeLab';
+  paths;
+
+  constructor() {
+    this.buildRoutes();
+  }
+
+  buildRoutes() {
+    this.paths = routes.map(route => ({
+      path: route.path,
+      name: route.path[0].toUpperCase() + route.path.slice(1)
+    }));
+  }
 }
